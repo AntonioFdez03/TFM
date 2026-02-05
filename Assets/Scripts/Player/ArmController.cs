8992addb5aa60475a2f60ae69d867fdd7f2375ef
AntonioFdez03 <antonio.fers2003@gmail.com>
2026-02-05 13:25:03 +0100

    punch damage

--- Assets/Scripts/Player/ArmController.cs
+++ Assets/Scripts/Player/ArmController.cs
@@ -8,6 +8,9 @@ public class ArmController : MonoBehaviour
     private float swingAngle = -40f;   // Ángulo de inclinación (X)
     private float swingDuration = 0.5f; // Velocidad de subida
     private float returnDuration = 0.1f; // Velocidad de bajada
+    private float punchDamage = 5f;
+    private float punchRange = 5f;
+    private Camera playerCamera;
     
     private Quaternion initialRotation;
     private bool isSwinging = false;
@@ -15,6 +18,7 @@ public class ArmController : MonoBehaviour
     void Start()
     {
         initialRotation = transform.localRotation;
+        playerCamera = Camera.main;
     }
 
     public void PlayAttackAnimation()
@@ -53,13 +57,29 @@ public class ArmController : MonoBehaviour
         // Aseguramos que vuelva exactamente a la posición original
         transform.localRotation = initialRotation;
         isSwinging = false;
-        UseItem();
+        SwingHit();
     }
 
-    private void UseItem()
+    private void SwingHit()
     {
         ItemBehaviour item = hotBarController.GetCurrentItemBehaviour();
         if(item != null)
             item.GetComponent<ItemBehaviour>().Use();
+        else
+            Punch();
+    }
+
+    private void Punch()
+    {
+        Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+        RaycastHit hit;
+
+        Debug.DrawRay(ray.origin, ray.direction * punchRange, Color.red);
+        if (Physics.Raycast(ray, out hit, punchRange))
+        {
+            Enemy enemy = hit.collider.CompareTag("Enemy") ? hit.collider.GetComponent<Enemy>() : null;
+            if (enemy != null)
+                enemy.TakeDamage(punchDamage);
+        }
     }
 }

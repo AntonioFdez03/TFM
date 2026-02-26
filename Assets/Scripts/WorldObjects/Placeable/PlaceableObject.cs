3498b5054265c399d38243c125c3df41a1593082
Antonio Fernández <afernandezsantiago03@gmail.com>
2026-02-26 13:14:07 +0100

    More items

---
+++ Assets/Scripts/WorldObjects/Placeable/PlaceableObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public abstract class PlaceableObject : MonoBehaviour
+{
+    public abstract void Place();
+}

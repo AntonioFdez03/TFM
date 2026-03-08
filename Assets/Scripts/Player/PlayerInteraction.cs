52cc12dcf1038c34c950aea86dff881adbd36710
AntonioFdez03 <antonio.fers2003@gmail.com>
2026-03-08 22:31:58 +0100

    Outline Shader Graph Working

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -11,6 +11,7 @@ public class PlayerInteraction : MonoBehaviour
 
     private InputAction interact;
     private InputAction attack;
+    private RaycastHit lastHit;
 
     void Start()
     {
@@ -32,7 +33,16 @@ public class PlayerInteraction : MonoBehaviour
         Ray ray = new Ray(CameraController.instance.transform.position, CameraController.instance.transform.forward);
         RaycastHit hit;
 
+        if(lastHit.collider != null)
+            SetChildrenLayer(lastHit.collider.gameObject, LayerMask.NameToLayer("Default"));
+
         bool hasHit = Physics.Raycast(ray, out hit, interactDistance);
+
+        if(hasHit)
+        {
+            if(hit.collider.CompareTag("Item"))
+                SetChildrenLayer(hit.collider.gameObject, LayerMask.NameToLayer("Outline"));
+        }
 
         if (HotBarController.instance.GetCurrentItemBehaviour() is PlaceableBehaviour placeable)
         {
@@ -52,18 +62,28 @@ public class PlayerInteraction : MonoBehaviour
             else if (hit.collider.CompareTag("Interactive"))
                 hit.collider.gameObject.GetComponent<InteractiveObject>().Interact();
         }
+
+        lastHit = hit;
     }
 
     private void Attack()
     {
         if (attack.triggered)
         {   
-            print("Current behaviour: " + HotBarController.instance.GetCurrentItemBehaviour());
             if (arm != null) 
                 arm.PlayAttackAnimation();
 
             if(HotBarController.instance.GetCurrentItemBehaviour() is PlaceableBehaviour placeable)
                 placeable.Use();
         }
+    }
+
+    private void SetChildrenLayer(GameObject item, LayerMask layer)
+    {   
+        print("Layer");
+        item.layer = layer;
+
+        foreach (Transform child in item.transform)
+            SetChildrenLayer(child.gameObject, layer);
     }
 }
